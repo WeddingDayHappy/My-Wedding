d4e6c49558fde9cdb5b7068013aad73e8c825c06
debiwandana18 <debiwandana03@gmail.com>
2024-12-23 15:38:24 +0700

    Add files via upload

---
+++ customs/Gsap.js
@@ -0,0 +1,8 @@
+gsap.registerPlugin(ScrollTrigger, TextPlugin);
+
+gsap.to('.hero p', {
+            duration: 2,
+            delay: 2,
+            text: 'Akan dilaksanakan dalam: ',
+        });
+
