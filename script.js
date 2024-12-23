b52dbf4ba6bef2f2c8e5ea4b04994137c2dcd978
debiwandana18 <debiwandana03@gmail.com>
2024-12-23 16:04:12 +0700

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 AOS.init();
+
+function copyAccountNumber(accountNumber) {
+    const textarea = document.createElement('textarea');
+    textarea.value = accountNumber;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    document.body.removeChild(textarea);
+    alert('Nomor rekening disalin: ' + accountNumber);
+}
+
