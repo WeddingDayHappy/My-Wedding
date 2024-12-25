33ea1bb1d71b030732d3e8bb50073ce6423e2876
WeddingDay <debiwandana03@gmail.com>
2024-12-25 16:06:27 +0700

    Gsap.js

--- customs/Gsap.js
+++ customs/Gsap.js
@@ -1,8 +1,41 @@
 gsap.registerPlugin(ScrollTrigger, TextPlugin);
 
 gsap.to('.hero p', {
-            duration: 2,
-            delay: 2,
-            text: 'Akan dilaksanakan dalam: ',
-        });
+    duration: 2,
+    delay: 2,
+    text: 'Akan dilaksanakan dalam: ',
+});
 
+const isiCerita = [
+    "Kami Bertemu pada tahun 2018, tepatnya saat kami menempuh pendidikan di Kampus yg sama. Siapa sangka pertemuan itu membawa kami pada suatu ikatan cinta yg suci.",
+    "Tidak ada yg kebetulan di dunia ini, semua sudah tersusun rapih oleh sang Maha Kuasa. Alhamdulillah yg telah mempertemukan kami dgn orang yg kami saling mencintainya hingga menempuh jalan yg diridhoinya.",
+    "Katanya Cinta dapat tumbuh dgn kebersamaan. Seiring berjalannya waktu, kami semakin dekat. Lalu kami berkomitmen pada tahun 2023 seakan terus berkonspirasi utk menyatukan kami berdua.",
+    "Menuntut kami pada sebuah pertemuan yg tak pernah disangka. Hingga akhirnya membawa kami pada sebua ikatan suci yg diridhoi-Nya. Kami melangsungkan acara lamaran pada 27 Oktober 2024."
+];
+
+// Pilih semua elemen <p> di dalam .timeline-heading
+const timeLineHeadingLists = document.querySelectorAll('.timeline-heading p');
+
+// Loop pertama untuk animasi GSAP
+for (let i = 0; i < timeLineHeadingLists.length; i++) {
+    gsap.to(timeLineHeadingLists[i], {
+        duration: 4,
+        delay: 3,
+        text: isiCerita[i],
+        scrollTrigger: {
+            start: "20% bottom",
+            end: "60% top",
+            trigger: timeLineHeadingLists[i],
+            scrub: true,
+            markers: true,
+        },
+    });
+}
+
+// Loop kedua untuk mengisi teks di elemen <p>
+for (let i = 0; i < timeLineHeadingLists.length; i++) {
+    const paragraph = timeLineHeadingLists[i];
+    if (paragraph) {
+        paragraph.textContent = isiCerita[i] || `teks ${i + 1}`; // Menggunakan textContent untuk menghilangkan <br>
+    }
+}
